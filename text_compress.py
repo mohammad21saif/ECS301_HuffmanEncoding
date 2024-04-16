bcb0e08fd1c2df409c2df8c65345cfdb8e67f7d8
mohammad21saif <mohammad21@iiserb.ac.in>
2024-04-16 21:52:06 +0530

    text_compress

--- text_compress.py
+++ text_compress.py
@@ -11,26 +11,24 @@ class Node:
     def __lt__(self, other):
         return self.freq < other.freq
 
+# Build the Huffman tree
 def build_huffman_tree(text):
-    # Count the frequency of each character in the text
-    frequency = Counter(text)
-    # Create a priority queue from the frequency table
-    priority_queue = [Node(char, freq) for char, freq in frequency.items()]
+    frequency = Counter(text) # Frequency of each character in the text
+    priority_queue = [Node(char, freq) for char, freq in frequency.items()]# Priority queue from the frequency table
     heapq.heapify(priority_queue)
     
     while len(priority_queue) > 1:
-        # Combine the two nodes with the lowest frequency
-        left = heapq.heappop(priority_queue)
+        left = heapq.heappop(priority_queue)# Combine the two nodes with the lowest frequency
         right = heapq.heappop(priority_queue)
 
-        # Create a new node with these two nodes as children and the combined frequency
-        merged = Node(None, left.freq + right.freq)
+        merged = Node(None, left.freq + right.freq)  # Create a new node with these two nodes as children and the combined frequency
         merged.left = left
         merged.right = right
         heapq.heappush(priority_queue, merged)
-
     return priority_queue[0]  # Return the root node of the Huffman tree
 
+
+# Assign Huffman codes to each character
 def assign_codes(node, prefix="", codebook={}):
     if node is not None:
         if node.char is not None:
@@ -39,12 +37,16 @@ def assign_codes(node, prefix="", codebook={}):
         assign_codes(node.right, prefix + "1", codebook)
     return codebook
 
+
+# Encode the text using the Huffman codes
 def huffman_encoding(text):
     root = build_huffman_tree(text)
     codebook = assign_codes(root)
     encoded_text = ''.join(codebook[char] for char in text)
     return encoded_text, codebook
 
+
+# Decode the encoded text using the Huffman codes
 def huffman_decoding(encoded_text, codebook):
     reverse_codebook = {code: char for char, code in codebook.items()}
     current_code = ""
@@ -58,9 +60,10 @@ def huffman_decoding(encoded_text, codebook):
 
     return decoded_text
 
-# Example usage
+
 if __name__ == "__main__":
     input_text = "The quick brown fox jumps over the lazy dog"
+    # input_text = input("Enter the text to be encoded: ")
     encoded_text, codebook = huffman_encoding(input_text)
     print("Encoded text:", encoded_text)
     print("Huffman Codes:", codebook)
